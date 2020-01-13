eb3a251f7ab5e20222cdfbc19c80543b06102b83
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-01-13 14:51:44 +0100

    Ajout  de essais/movie -durationToString

--- essais/movie.py
+++ essais/movie.py
@@ -4,6 +4,6 @@ def durationToString(minutes: int) -> str:
 Convertit une durée exprimée en minutes en une chaîne de caractères ayant la forme HH:MM
 Si nécessaire, le nombre d'heures peut comporter plus de 2 chiffres.
 @param $minutes durée en minutes
-@return résultat*/
+@return résultat
     """
-    return "à faire"
+    return f"{minutes//60:02d}:{minutes%60:02d}"
