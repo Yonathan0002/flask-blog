0377f28b18571684d6968d8ef81e304018619411
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-01-20 15:31:35 +0100

    push de fin de sceance

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+# Création de l’application :
+app = Flask( __name__ )
+# Définition d’une fonction de "vue" accrochée
+# au lien / (route)
+
+if __name__ == "__main__" :
+    app.run()
+
+@app.route("/")
+def hello() -> str:
+    return """
+    <!doctype html>
+    <html lang="fr">
+    <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Ma première page Flask.">
+    <meta name="keywords" content="Flask">
+    <meta name="author" content="moi-même">
+    <title>Ma première page Flask</title>
+    </head>
+    <body>
+    <h1>Ma première page Flask</h1>
+    <p>Flask est vraiment super !</p>
+    <hr/>
+    </body>
+    </html>
+    """
+    
