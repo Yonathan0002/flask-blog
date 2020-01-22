a48d112ab2eed02b8c20fa41039267edc420c2c7
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-01-22 11:47:16 +0100

    push de fin de sceance

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,21 @@
 from flask import render_template
-from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = { 'username' : 'Philippe' }
-    return render_template('index.html', title='Page principale', user=user)
+    #return render_template('index.html', user=user)
+
+    posts = [
+        {
+            'author': {'username' : 'John'},
+            'body' : "coucou"
+        },
+        {
+            'author': {'usernamer':'fred'},
+            'body': "salut"
+        }
+    ]
+
+    return render_template('index.html', title='Accueil',user=user,posts=posts)
--- hello.py
+++ hello.py
@@ -5,27 +5,26 @@ appli = Flask( __name__ )
 # Définition d’une fonction de "vue" accrochée
 # au lien / (route)
 
+@appli.route("/")
+def hello() -> str:
+    return """
+    <!doctype html>
+    <html lang="fr">
+    <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Ma première page Flask.">
+    <meta name="keywords" content="Flask">
+    <meta name="author" content="moi-même">
+    <title>Ma première page Flask</title>
+    </head>
+    <body>
+    <h1>Ma première page Flask</h1>
+    <p>Flask est vraiment super !</p>
+    <hr/>
+    </body>
+    </html>
+    """
+
 if __name__ == "__main__" :
     appli.run()
 
-@appli.route("/")
-def hello() -> str:
-    pass
-    # return """
-    # <!doctype html>
-    # <html lang="fr">
-    # <head>
-    # <meta charset="utf-8">
-    # <meta name="description" content="Ma première page Flask.">
-    # <meta name="keywords" content="Flask">
-    # <meta name="author" content="moi-même">
-    # <title>Ma première page Flask</title>
-    # </head>
-    # <body>
-    # <h1>Ma première page Flask</h1>
-    # <p>Flask est vraiment super !</p>
-    # <hr/>
-    # </body>
-    # </html>
-    # """
-    
