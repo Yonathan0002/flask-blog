004306d5773899abf0fcc06f1998f26ab9bbde0e
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-01-29 11:47:43 +0100

    fin du tp5

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'username' : 'Philippe' }
+    user = { 'username' : 'Yonathor' }
     #return render_template('index.html', user=user)
 
     posts = [
@@ -18,8 +20,21 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='yoyo',user=user,posts=posts)
+    return render_template('index.html', title='yoyo', user=user, posts=posts)
 
 @app.route('/apropos')
 def apropos():
     return render_template('apropos.html',title='yoyo')
+
+# @app.route('/login')
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', title='Enregistrement', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form= LoginForm()
+    if form.validate_on_submit():
+        flash(f"Enregistrement demandé pour l’utilisateur {form.username.data} (Remember is {form.remember_me.data})")
+        return redirect('/index')
+    return render_template('login.html', title='Enregistrement', form=form)
