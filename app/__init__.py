e50252bd722e6fbbacd17133c45f6bb060a98952
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-03 15:32:28 +0100

    tp6 question25

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 #creation de l'application
+# __name__ contient le nom de l'application : app
 app= Flask( __name__ ) 
 app.config.from_object(Config)
-# On importe le fichier contenant 
-# # la définitiondes fonctions de vue
-from app import routes
 
+# Démarrage du moteur de la base de données
+db = SQLAlchemy(app)
+# Démarrage de l'outil de migration associé à la base de données
+migrate = Migrate(app, db)
+# On importe le fichier contenant
+# la définition des fonctions de vue
+# ainsi que celui des modèles
+from app import routes, models
