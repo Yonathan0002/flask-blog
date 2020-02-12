9a499f37382b377faa681c2a1de39f601d2c8e19
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-12 11:46:00 +0100

    tp se connecter exercice 23

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError, Email
+from app.models import User
 class LoginForm(FlaskForm) :
     username = StringField('Utilisateur', validators=[DataRequired()])
     password = PasswordField('Mot de passe', validators=[DataRequired()])
