ba135320bd89460f7f0f6c53a71d23ee68a8b7fc
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-17 14:24:45 +0100

    Fin de la partie connexion

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,20 @@ class LoginForm(FlaskForm) :
     remember_me = BooleanField('Se souvenir de moi')
     submit = SubmitField('Enregistrer')
 
+class RegistrationForm(FlaskForm):
+    username = StringField(label='Utilisateur', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Mot de passe', validators=[DataRequired()])
+    password2 = PasswordField(label='Répéter le mot de passe', \
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Enregistrer")
+
+    def validate_username(self: object, username: StringField) -> None:
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Choisissez un autre nom.")
+            
+    def validate_email(self: object,email: StringField) -> None:
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Choisissez une autre adresse Email.")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,8 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
 from app.models import User
-from app.forms import LoginForm
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user,login_user
-from flask_login import login_required
-from flask import request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user,login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 @app.route('/')
@@ -65,3 +62,17 @@ from flask_login import current_user, login_user, logout_user
 def logout() -> str:
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register() -> str:
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Bravo ! Vous devenez un nouvel utilisateur !')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="S'enregistrer", form=form)
