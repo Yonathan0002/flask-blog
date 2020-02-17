09269046add51f6173f6e908de7e97b96b17f06d
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-17 15:21:11 +0100

    Ajout des avatars

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +12,10 @@ class User(db.Model, UserMixin):
 
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def avatar(self: object, size: int) -> str:
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
     posts=db.relationship('Post', backref='author', lazy='dynamic')
     def __repr__(self: object) -> str:
