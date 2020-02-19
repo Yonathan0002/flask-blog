b9b92e368fef23b98e50813ba990b738fb20976d
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-19 11:03:10 +0100

    Ajout du gestionnaire de mail pour les erreurs

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,32 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
+
+import logging
+from logging.handlers import SMTPHandler
+
 #creation de l'application
 # __name__ contient le nom de l'application : app
 app= Flask( __name__ ) 
 app.config.from_object(Config)
+
+# Chargement des variables de configuration depuis la classe Config
+app.config.from_object(Config)
+
+# Mise en place d'un gestionnaire de mails pour les messages d'erreur
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],toaddrs = app.config['ADMINS'], subject='Erreur dans MonApplication',credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 
 # Démarrage du moteur de la base de données
 db = SQLAlchemy(app)
@@ -27,3 +49,6 @@ from app import routes, models
 # de l’application et des erreurs
 # ainsi que celui des modèles
 from app import routes, models, erreurs
+
+
+
--- app/config.py
+++ app/config.py
@@ -7,3 +7,11 @@ class Config:
     # Configuration de la base de données
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configuration d'un "pseudo" serveur mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [ 'youremail@exemple.com' ]
