1d5b1116285ff193a431cc860096156db2c5fbba
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-19 11:03:45 +0100

    Gestion serveur université

--- app/config.py
+++ app/config.py
@@ -14,4 +14,4 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = [ 'youremail@exemple.com' ]
+    ADMINS = [ 'yonathan.piat@etudiant.univ-reims.fr' ]
