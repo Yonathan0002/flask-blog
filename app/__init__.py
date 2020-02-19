67c4194705af742f75fda54a732aab3b5e6ddbae
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-02-19 11:25:16 +0100

    Ajout  de  la  gestion  des messages par fichier.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ from flask_login import LoginManager
 
 
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 #creation de l'application
 # __name__ contient le nom de l'application : app
@@ -17,18 +18,30 @@ app.config.from_object(Config)
 app.config.from_object(Config)
 
 # Mise en place d'un gestionnaire de mails pour les messages d'erreur
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],toaddrs = app.config['ADMINS'], subject='Erreur dans MonApplication',credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+# if not app.debug:
+#     if app.config['MAIL_SERVER']:
+#         auth = None
+#         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+#             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+#         secure = None
+#         if app.config['MAIL_USE_TLS']:
+#             secure = ()
+#         mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],toaddrs = app.config['ADMINS'], subject='Erreur dans MonApplication',credentials=auth, secure=secure)
+#         mail_handler.setLevel(logging.ERROR)
+#         app.logger.addHandler(mail_handler)
 
+# Ajout du gestionnaire de messages permettant de stocker
+# les messages dans un ou plusieurs fichiers.
+if not app.debug:
+    # Création du répertoire pour les fichiers de log
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/mon_application.log', maxBytes=512000 ,backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s : %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Démarrage de MonApplication')
 
 # Démarrage du moteur de la base de données
 db = SQLAlchemy(app)
