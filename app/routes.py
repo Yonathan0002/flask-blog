7c6589aa72c6d108e6b97b9a6280d86209519e64
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-03-04 11:48:59 +0100

    tp pagination exercice 4

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.models import User
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.models import User, Post
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user,login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
@@ -12,22 +12,19 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 @login_required
 def index():    
-    posts = [
-        {
-            'author': {'username' : 'John'},
-            'body' : "coucou"
-        },
-        {
-            'author': {'username': 'suzan'},
-            'body': "salut"
-        }
-    ]
-
-    return render_template('index.html', title='Accueil', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Votre message est maintenant en ligne !")
+        return redirect(url_for('index'))
+    posts = current_user.post_abonnes().all()
+    return render_template('index.html', title='Accueil', form=form, posts=posts)
 
 @app.route('/apropos')
 def apropos():
