cadeda510f188282633b002d04d08a6775c2ab1f
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-03-11 11:41:27 +0100

    probleme avec le PostForm

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, ValidationError, Email, Length
 from app.models import User
-
 class LoginForm(FlaskForm) :
     username = StringField('Utilisateur', validators=[DataRequired()])
     password = PasswordField('Mot de passe', validators=[DataRequired()])
@@ -31,3 +30,9 @@ class EditProfileForm(FlaskForm):
     username = StringField(label='Utilisateur', validators=[DataRequired()])
     about_me = StringField(label='A propos de moi', validators=[Length(min=0, max=140)])
     submit = SubmitField("Sauvegarder")
+
+class PostForm(FlaskForm):
+    post = TextAreaField(label="Message du Post", validators = [DataRequired(message="Un message est nécessaire."), Length(min=1, max=140, message="Le message doit comprendre entre %(min)d et %(max)d caractères.")])
+    submit = SubmitField(label="Enregistrer")
+
+
